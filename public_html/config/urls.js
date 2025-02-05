8dcdae6d6f8a92047a2b7e220438d65dbdf9aa14
ianmiguelsoler <ianmiguel@gmail.com>
2025-02-05 21:21:32 +0000

    subida inicial front

---
+++ public_html/config/urls.js
@@ -0,0 +1,3 @@
+// Configurar la URL de la API Backend
+const apiUrl = "http://localhost:8080";  // En desarrollo
+// const apiUrl = "http://tu-dominio-en-aws.com";  // Para producción
--- urls.js
+++
@@ -1,2 +0,0 @@
-//Rutas de las llamadas HTTP
-let apiUrl = "";
