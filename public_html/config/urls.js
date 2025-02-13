b390bbc4467f29b4a2ed6bb5822a129e3c008fd2
ianmiguelsoler <ianmiguel@gmail.com>
2025-02-13 22:43:11 +0000

    entorno de produccion

--- public_html/config/urls.js
+++ public_html/config/urls.js
@@ -1,5 +1,5 @@
 const apiUrl = window.location.hostname.includes("localhost")
-    ? "http://localhost:8080"  // Desarrollo
-    : "http://peliculasian.chickenkiller.com";  // Producción
+    ? "http://localhost:8080"  // 🔹 Desarrollo
+    : "http://peliculasian.chickenkiller.com";  // 🔹 Producción
 
 console.log(`🔹 API URL: ${apiUrl}`);
